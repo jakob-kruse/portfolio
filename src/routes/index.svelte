40dba260fdf50d528fa71235096c182e3044f7da
Jakob Kruse <jakob.kruse@tuta.io>
2020-06-05 18:33:25 +0200

    Added Ackee Tracking (dont hate me)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,16 @@
 <script>
+    import { onMount } from 'svelte';
     import SectionTop from '../components/SectionTop.svelte'
     import SectionBottom from "../components/SectionBottom.svelte";
+
+    onMount(async () => {
+        const ackeeTracker = (await import('ackee-tracker')).default;
+
+        ackeeTracker.create({
+            server: 'https://ackee.lastdigitsofpi.dev',
+            domainId: '5eea4c0f-e468-47eb-8b18-c3ec9f292bc4'
+        }).record()
+    });
 
     function scrollDown() {
         document.getElementsByClassName("section--bottom")[0].scrollIntoView({behavior: "smooth"});
